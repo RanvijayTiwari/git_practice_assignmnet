1cfd5af21ffceece1b3469f4d59b1ecbdb10e01b
ranvi_2mvnuyu <ranvijaytiwari0000.com>
2022-12-01 10:05:33 +0530

    written but not correct way

---
+++ palindromeproblem.js
@@ -0,0 +1,3 @@
+let pali="heloo";
+for(let i=0; i<pali; i++){
+bag+=pali[i];}
---
+++ primeproblem.js
@@ -0,0 +1,5 @@
+let prime=13;
+for(let i=0; i<prime; i++){
+if(i%prime==0){
+console.log("prime")}else{
+console.log("No")}
